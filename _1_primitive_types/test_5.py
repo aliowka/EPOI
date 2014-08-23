c7ab6065920ef2a6879584b36df0f45d765153d1
aliowka <aliowka@home>
2014-08-23 04:02:47 +0300

    reverse bits added

--- _1_primitive_types/test_5.py
+++ _1_primitive_types/test_5.py
@@ -5,6 +5,8 @@ Created on Apr 12, 2014
 '''
 import time
 import unittest
+import math
+import random
 
 class Test(unittest.TestCase):
 
@@ -40,11 +42,11 @@ class Test(unittest.TestCase):
             self.precomputed[(x >> 48) & 0b1111111111111111]
  
     def test_parity_1(self):
-        self.assertEqual( self.parity_1(0b1),     1 )
-        self.assertEqual( self.parity_1(0b11),    0 )
-        self.assertEqual( self.parity_1(0b101),   0 )
-        self.assertEqual( self.parity_1(0b111),   1 )
-        self.assertEqual( self.parity_1(0b1101),  1 )
+        self.assertEqual(self.parity_1(0b1), 1)
+        self.assertEqual(self.parity_1(0b11), 0)
+        self.assertEqual(self.parity_1(0b101), 0)
+        self.assertEqual(self.parity_1(0b111), 1)
+        self.assertEqual(self.parity_1(0b1101), 1)
  
     def test_parity_2(self):
         self.precompute()
@@ -56,11 +58,11 @@ class Test(unittest.TestCase):
         self.precompute()
          
         st = time.time()
-        for i in xrange(1<<20):
+        for i in xrange(1 << 20):
             self.parity_1(i)
         print "Raw parity", time.time() - st
         st = time.time()
-        for i in xrange(1<<20):
+        for i in xrange(1 << 20):
             self.parity_2(i)
         print "Cached parity", time.time() - st
          
@@ -69,14 +71,68 @@ class Test(unittest.TestCase):
         if (x >> i) & 1 == (x >> j) & 1:
             return x
         
-        x = x ^ (1 << i)
-        x = x ^ (1 << j)
+        x ^= (1 << i) | (1 << j)
         return x
         
     def test_swap_bits(self):
         self.assertEqual(self.swap_bits(0b10101, 1, 0), 0b10110)
         self.assertEqual(self.swap_bits(0b10101, 2, 0), 0b10101)
         self.assertEqual(self.swap_bits(0b10101, 2, 3), 0b11001)
+
+    def reverse_bits_with_shift(self, x):
+        res = 0
+        for i in xrange(65):
+            bit = x & 1
+            x >>= 1
+            res = (res << 1) + bit
+        return res
+        
+    def reverse_bits_with_swap(self, x):
+        for i in xrange(32):
+            x = self.swap_bits(x, i, 64 - i)
+        return x
+        
+    def reverse_bits_with_log(self, x):
+        res = 0
+        while x:
+            bit = x & ~(x - 1)
+            x = x & (x - 1)
+            offset = 64 - int(math.log(bit, 2))
+            res |= (1 << offset)
+        return res
+        
+    def test_reverse_bit(self):
+        self.assertEqual(self.reverse_bits_with_shift(1), 2 ** 64)
+        self.assertEqual(self.reverse_bits_with_shift(2), 2 ** 63)
+        self.assertEqual(self.reverse_bits_with_swap(1), 2 ** 64)
+        self.assertEqual(self.reverse_bits_with_swap(2), 2 ** 63)
+        self.assertEqual(self.reverse_bits_with_log(1), 2 ** 64)
+        self.assertEqual(self.reverse_bits_with_log(2), 2 ** 63)
+        
+        for i in xrange(100):
+            x = random.randint(0, 2 ** 63)
+            self.assertEqual(self.reverse_bits_with_log(x) ==\
+                             self.reverse_bits_with_shift(x) ==\
+                             self.reverse_bits_with_swap(x), True)
+        
+        bulk_size = 10000
+
+        st = time.time()
+        for i in xrange(bulk_size):
+            self.reverse_bits_with_shift(random.randint(0, 2 ** 63))
+        print "Reverse bits with shift:", (time.time() - st)
+
+        st = time.time()
+        for i in xrange(bulk_size):
+            self.reverse_bits_with_swap(random.randint(0, 2 ** 63))
+        print "Reverse bits with swap:", (time.time() - st)
+
+        st = time.time()
+        for i in xrange(bulk_size):
+            self.reverse_bits_with_log(random.randint(0, 2 ** 63))
+        print "Reverse bits with log:", (time.time() - st)
+        
+
         
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testParity']
